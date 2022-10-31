7549ccf1821bc95fe3d002c0058404e958322714
new_account <sheltowt@gmail.com>
2022-10-31 14:55:54 -0600

    debug = false

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def display_resume():
 		return make_response(jsonify(resume), 200)
 
 if __name__ == '__main__':
-    basic_api.run(debug=True, host='0.0.0.0', port=8080)
+    basic_api.run(debug=False, host='0.0.0.0', port=8080)
